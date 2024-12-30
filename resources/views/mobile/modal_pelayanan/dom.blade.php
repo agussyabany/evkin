83007413cad3b1fff32fc61415b83c930ee376a2
agussyabany <agussyabany@gmail.com>
2024-12-31 07:49:30 +0800

    Dom Lable modal domestik

--- resources/views/mobile/modal_pelayanan/dom.blade.php
+++ resources/views/mobile/modal_pelayanan/dom.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-xl modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h4 class="text-center" id="judul">KONSUMSI ARI DOMESTIK</h4>
+          <h4 class="text-center" id="judul">KONSUMSI AIR DOMESTIK</h4>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
